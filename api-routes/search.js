aa72daf13fa6b5c2e7cde24b1d80ab2568282152
ashishdutta007 <ashish.dutta050@gmail.com>
2017-03-27 15:02:10 +0530

    Bing search enabled

--- api-routes/search.js
+++ api-routes/search.js
@@ -5,16 +5,14 @@ var dbops = require('../db/mongo.js');
 //Creating a router object - like a mini-express object
 //Can be used for routing,logging,request param extraction for validation
 var searchApi = express.Router();
+var bing_apikey = '6f3df627fdd64daab3c97a10620903b0';
 //npm bing-search api module 
-//var bing_search = require('bing.search');
-var bing_search = require('node-bing-api').({ accKey: 'ecd9d839f6824164b5dd8c5a47600b5d' });
-var bing_apikey = 'ecd9d839f6824164b5dd8c5a47600b5d';
-//console.log('Inside search.js');
+var nodeBingApi = require("node-bing-api")({ accKey: bing_apikey });
+console.log('Inside search.js');
 
 //Route middleware that will happen on every request
 //Logging every request
 searchApi.use(function(request, response, next) {
-    response.send("Inside Search API");
     request.on('error', function(error) {
         return console.log('Error occurrred', error);
     });
@@ -32,35 +30,44 @@ searchApi.get('/imagesearch/:term', function(request, response, next) {
     var timestamp = Date.now();
     console.log("timestamp: ", timestamp);
     var searchData = {
-            'searchterm': searchterm,
-            'timestamp': timestamp
-        }
-        //save new search data to mongodb
+        'searchterm': searchterm,
+        'timestamp': timestamp
+    };
+
+    //save new search data to mongodb
     dbops.savesearchterm(searchData)
         .then(function(docs) {
             console.log("Docs: ", docs);
             return console.log('Data saved successfully to db');
         }).catch(function(error) {
+            response.status(500).json(error);
             return console.log('Data save failed');
         });
 
-
-    //call bing search API code
-    var search = new bing_search(bing_apikey);
-    search.images(searchterm, { top: 5 }, function(error, results, body) {
+    //call bing search API
+    nodeBingApi.images(searchterm, { top: 1 }, function(error, results, body) {
         if (error) {
+            response.status(500).json(error);
             return console.log('An error occurred', error);
-            response.send('Error');
         } else {
-            console.log('Search results body: ', body);
+            var returnResponse = {
+                'webSearchURL': body.webSearchUrl,
+                'hostPageURL': body.value[0].hostPageUrl,
+                'altName': body.value[0].name,
+                'suggestedURL': body.pivotSuggestions[0].suggestions[0].webSearchUrl,
+                'suggestedAltName': body.pivotSuggestions[0].suggestions[0].text
+            };
+            response.status(200).json(returnResponse);
         }
     });
 
     //handling error events
     request.on('error', function(error) {
+        response.status(500).json(error);
         return console.log('Error occurrred', error);
     });
     response.on('error', function(error) {
+        response.status(500).json(error);
         return console.log('Error occurrred', error);
     });
 });
