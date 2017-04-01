64a28de8537194e6d629b0393bd3a2ad557f9318
ashishdutta007 <ashish.dutta050@gmail.com>
2017-04-01 09:33:42 +0530

    mLab url changes

--- db/mongo.js
+++ db/mongo.js
@@ -3,7 +3,8 @@ var mongodb = require('mongodb');
 var mongoose = require('mongoose');
 var model = require('../models/searchHistoryModel.js');
 //MONGOLAB_URI for heroku-mLab deployment
-var url = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/searcHistory';
+var url = process.env.MONGOLAB_URI;
+//var url = 'mongodb://localhost:27017/searcHistory';
 
 console.log('Inside mongo.js');
 
